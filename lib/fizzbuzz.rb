78f9a35c010d3b3a96902c61f2cf34b99e24c4c2
nicolasraffray <nraffray@gmail.com>
2019-12-03 15:01:11 +0000

    adding git file

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 != 0 
+    'fizz'
+  elsif number % 3 != 0 && number % 5 == 0
+    'buzz'
+  elsif number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  else 
+    number
+  end
+end
+
+(1..20).each do |x|
+  puts "#{x} --> #{fizzbuzz(x)}"
+end
